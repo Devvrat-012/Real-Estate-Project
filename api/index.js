3185f315b9989ca94a3190499a59c88923b755cc
Dev Stark <starkji555@gmail.com>
2024-01-26 16:44:29 +0530

    Created a middleware and a function to handle the possible errors.

--- api/index.js
+++ api/index.js
@@ -20,6 +20,17 @@ app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter)
 
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error!';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
+
+
 app.listen(port, ()=>{
     console.log(`Your app is running on the port ${port}`)
 });
