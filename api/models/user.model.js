238e1059e683308feb03b26005a1d87e100ec8d3
Dev Stark <starkji555@gmail.com>
2024-01-30 12:38:40 +0530

    Created and Added OAuth Component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,7 +16,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,        
-    }
+    },
+    avatar: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtfujKds3Q_OKa076KItTTIrjQYIeq0-KqDA&usqp=CAU"
+    },
   
 }, {timestamps:true});
 
